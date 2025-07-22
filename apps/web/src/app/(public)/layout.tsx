2e73e17cf8bc9bcad5b910694e4779d0528aa3e7
Sander Verkuil <s.verkuil@pm.me>
2025-07-22 13:11:11 +0200

    Update layout.tsx

--- apps/web/src/app/(public)/layout.tsx
+++ apps/web/src/app/(public)/layout.tsx
@@ -9,7 +9,7 @@ export default async function DashboardLayout({
   return (
     <div className="bg-background pt-[57px]">
       <Header />
-      <div className="container mx-auto py-6">{children}</div>
+      <div className="container mx-auto p-6">{children}</div>
       <Footer />
     </div>
   );
