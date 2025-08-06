d60d4991002532d08f7a8aed98e50dffb51192af
Sander Verkuil <s.verkuil@pm.me>
2025-08-06 09:25:00 +0200

    If the product is null, it's not found

--- apps/web/src/app/(dashboard)/puzzles/products/[id]/_components/puzzle-product-detail.tsx
+++ apps/web/src/app/(dashboard)/puzzles/products/[id]/_components/puzzle-product-detail.tsx
@@ -34,6 +34,10 @@ export function PuzzleProductDetail({ productId }: PuzzleProductDetailProps) {
       : "skip",
   );
 
+  if (product === null) {
+    return <h1>Product not found</h1>;
+  }
+
   if (
     product === undefined ||
     (product.category !== undefined && category === undefined)
